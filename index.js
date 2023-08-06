082e017f79595c83efb651752aab015d71743027
syedaffanahmed12 <syedaffanahmed12@gmail.com>
2023-08-06 11:08:03 +0500

    changes

---
+++ index.js
@@ -0,0 +1,19 @@
+for(let i=0 ; 1<0 ;i++)
+{
+    console.log(i)
+}
+let i=0;
+while(i<6)
+{
+    console.log (i)
+    i++;
+}
+while(i<10)
+{
+    if (i%2==0)
+    {
+        console.log(i)
+    }
+    i++;
+}
+
